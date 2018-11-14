Trying to fix backprop of MahalanobisLayer

modules/autoencoder.py
@@ -35,8 +35,8 @@ class Autoencoder(nn.Module):
     def forward(self, x):
         x_enc = self.encoding_layers(x)
         x_fit = self.decoding_layers(x_enc)
-        #err = self.mahalanobis.distance(x_in, x_fit)
-        return x_fit
+        err = self.mahalanobis(x, x_fit)
+        return err
 
 
 if __name__ == "__main__":
@@ -45,32 +45,28 @@ if __name__ == "__main__":
     N, D_in, H1, H2, H3 = 64, 20, 100, 10, 100
 
     # Create random Tensors to hold inputs and outputs
-    x = torch.randn(N, D_in)
+    x = torch.Tensor(torch.randn(N, D_in))
 
     # Construct our model by instantiating the class defined above
     model = Autoencoder(D_in, H1, H2, H3)
 
     # Select device to train model on and copy model to device
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    #device = "cpu"
     model.to(device)
 
     # Also copy data to device, when training on real data this should be done for each mini-batch
     x = x.to(device)
 
-    # Construct our loss function and an Optimizer. The call to model.parameters()
-    # in the SGD constructor will contain the learnable parameters of the two
-    # nn.Linear modules which are members of the model.
+    # Construct our loss function and an optimizer
     criterion = torch.nn.MSELoss(reduction='sum')
-    optimizer = torch.optim.Adam(model.parameters(), lr=1e-5)
+    optimizer = torch.optim.Adam(model.parameters())
 
-    for t in range(10000):
+    for t in range(10):
         # Forward pass: Compute predicted y by passing x to the model
         x_fit = model(x)
 
         # Compute and print loss
-        #loss = criterion(x_fit, torch.zeros(x.size(0), device=device))
-        loss = criterion(x_fit, x)
+        loss = criterion(x_fit, torch.zeros(N, device=device)) #x_fit.size(), device=device))
         print(t, loss.item())
 
         # Zero gradients, perform a backward pass, and update the weights.

modules/mahalanobis.py
@@ -5,7 +5,6 @@ Mahalanobis module
 """
 import torch
 import torch.nn as nn
-from torch.nn.parameter import Parameter
 
 class MahalanobisLayer(nn.Module):
 
@@ -13,20 +12,21 @@ class MahalanobisLayer(nn.Module):
         super(MahalanobisLayer, self).__init__()
         self.register_buffer('S', torch.eye(dim))
         self.register_buffer('S_inv', torch.eye(dim))
+        #self.S = torch.eye(dim, requires_grad=False)
+        #self.S_inv = torch.eye(dim, requires_grad=False)
         self.decay = decay
-        self.training = False
 
-    def cov(self, x):
-        x -= torch.mean(x, dim=0)
-        return 1 / (x.size(0) - 1) * x.t().mm(x)
-
-    def distance(self, x, x_fit):
+    def forward(self, x, x_fit):
         """
         Calculates Mahalanobis distance between x and x_fit
         """
         delta = x - x_fit
         m = torch.mm(torch.mm(delta, self.S_inv), delta.t())
         return torch.diag(torch.sqrt(m))
+
+    def cov(self, x):
+        x -= torch.mean(x, dim=0)
+        return 1 / (x.size(0) - 1) * x.t().mm(x)
 
     def update(self, x, x_fit):
         delta = x - x_fit
@@ -51,7 +51,28 @@ if __name__ == "__main__":
     mah_layer = MahalanobisLayer(3, decay=0.99)
     mah_layer.S_inv = iv
 
-    pytorch_dist = mah_layer.distance(X1, X2)
+    pytorch_dist = mah_layer(X1, X2)
+
+    criterion = torch.nn.MSELoss(reduction='sum')
+
+    a = torch.tensor(X2, requires_grad=True)
+    print(X1.requires_grad)
+    print(a.requires_grad)
+    #new_mah_layer = MahalanobisLayer(3)
+    #b = new_mah_layer(X1, a)
+    #delta = X1 - a
+    #m = torch.mm(torch.mm(delta, iv), delta.t())
+    #d =  torch.diag(torch.sqrt(m))
+    a = torch.mm(a, X1)
+    b = criterion(a, torch.zeros(3,3))
+
+    #b = torch.sum(d)
+    print(b)
+    print(a)
+    b.backward()
+    print(b.grad)
+    print(a.grad)
+    #print(d.grad)
 
     # Check if almost equal
     np.testing.assert_almost_equal(scipy_dist, pytorch_dist.numpy())